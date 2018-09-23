8a81e76ef32c33242626bf1a4f52a46cf38d3d7a
sofiamv <league@WTS-IM-6.attlocal.net>
2018-09-23 12:58:07 -0700

    help

--- src/_00_Sorting_Algorithms/MergeSorter.java
+++ src/_00_Sorting_Algorithms/MergeSorter.java
@@ -13,14 +13,44 @@ public class MergeSorter extends Sorter {
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
 		//20. call the mergeSort method with 0 and the length of the array minus one
+		mergeSort(array, 0, array.length-1, display);
 	}
 	
 	private void mergeSort(int[] array, int low, int high, SortingVisualizer display) {
 		//1. Create a temporary integer array that is the same length as the passed in array.
-		
+			Integer[] temp = new Integer[array.length];
 		//2. make an if statement that checks if low is less than high
 		//   and put the rest of the method inside of it
-
+			if(low<high) {
+				int middle = (low+high)/2;
+				mergeSort(array, low, middle, display);
+				mergeSort(array, middle+1, high, display);
+				for (int i = low; i <= high; i++) {
+					temp[i]=array[i];
+				}
+				int i = low;
+				int j = middle+1;
+				int k = low;
+				while(i<=middle && j<=high) {
+					if(temp[i] <=temp[j]) {
+						array[k] = temp[i];
+						display.updateDisplay();
+						i++;
+					}else {
+						array[k] = temp[j];
+						display.updateDisplay();
+						j++;
+					}
+					k++;
+				}
+				while(i<=middle) {
+					array[k] = temp[i];
+					display.updateDisplay();
+					k++;
+					i++;
+				}
+				display.updateDisplay();
+			}
 			//3. Create an integer called middle and set it 
 			//   equal to the half way
             
@@ -28,7 +58,7 @@ public class MergeSorter extends Sorter {
            
             //5. call the mergeSort method with middle + 1 and high
             
-            //6. copy the element from the array into the temporary array,
+            //6. copy the elements from the array into the temporary array,
             //   but only the elements from low to high inclusive
             
             //7. create three integers called i, j, and k and
